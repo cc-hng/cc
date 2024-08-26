56d2d73556f131963dc735a9f1e742508df8e068
cc.h <cc@l2x.top>
2024-08-26 16:54:58 +0800

    Update

--- include/cc/stopwatch.h
+++ include/cc/stopwatch.h
@@ -11,6 +11,9 @@ class StopWatch {
 
 public:
     StopWatch() : start_tp_{clock::now()} {}
+    StopWatch(int ago) : start_tp_(clock::now() - std::chrono::seconds(ago)) {}
+    template <typename Duration>
+    StopWatch(Duration& duration) : start_tp_(clock::now() - duration) {}
 
     double elapsed() const {
         return std::chrono::duration<double>(clock::now() - start_tp_).count();

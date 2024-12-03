3d8148ebc969512ab1d179b95390fc1e68e08f8a
cc <cc@l2x.top>
2024-12-03 14:36:32 +0800

    Update

--- cmake/common.cmake
+++ cmake/common.cmake
@@ -29,14 +29,6 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "Clang" OR CMAKE_CXX_COMPILER_ID STREQUAL "App
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++")
 endif()
 
-# ASAN
-if(CC_ENABLE_ASAN
-   AND NOT CMAKE_CROSSCOMPILING
-   AND NOT WIN32)
-  set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_CXX_FLAGS_DEBUG} -fsanitize=address -fsanitize=undefined")
-  set(CMAKE_EXE_LINKER_FLAGS_DEBUG "${CMAKE_EXE_LINKER_FLAGS_DEBUG} -fsanitize=address -fsanitize=undefined")
-endif()
-
 # lto
 include(CheckIPOSupported)
 macro(link_time_optimization)

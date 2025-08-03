70825b762d9016c673e94512dd8b98c0daf66ed0
cc <cc@l2x.top>
2025-08-03 21:56:48 +0800

    Update

--- cmake/cpm.cmake
+++ cmake/cpm.cmake
@@ -1,9 +1,17 @@
 # SPDX-License-Identifier: MIT
 #
 # SPDX-FileCopyrightText: Copyright (c) 2019-2023 Lars Melchior and contributors
+if(DEFINED ENV{CPM_VERSION})
+  set(CPM_DOWNLOAD_VERSION $ENV{CPM_VERSION})
+else()
+  set(CPM_DOWNLOAD_VERSION 0.42.0)
+endif()
 
-set(CPM_DOWNLOAD_VERSION 0.40.8)
-set(CPM_HASH_SUM "78ba32abdf798bc616bab7c73aac32a17bbd7b06ad9e26a6add69de8f3ae4791")
+if(DEFINED ENV{CPM_HASHSUM})
+  set(CPM_HASHSUM $ENV{CPM_HASHSUM})
+else()
+  set(CPM_HASHSUM "2020b4fc42dba44817983e06342e682ecfc3d2f484a581f11cc5731fbe4dce8a")
+endif()
 
 if(CPM_SOURCE_CACHE)
   set(CPM_DOWNLOAD_LOCATION "${CPM_SOURCE_CACHE}/cpm/CPM_${CPM_DOWNLOAD_VERSION}.cmake")
@@ -18,7 +26,7 @@ get_filename_component(CPM_DOWNLOAD_LOCATION ${CPM_DOWNLOAD_LOCATION} ABSOLUTE)
 
 file(DOWNLOAD
      https://github.com/cpm-cmake/CPM.cmake/releases/download/v${CPM_DOWNLOAD_VERSION}/CPM.cmake
-     ${CPM_DOWNLOAD_LOCATION} EXPECTED_HASH SHA256=${CPM_HASH_SUM}
+     ${CPM_DOWNLOAD_LOCATION} EXPECTED_HASH SHA256=${CPM_HASHSUM}
 )
 
 include(${CPM_DOWNLOAD_LOCATION})
